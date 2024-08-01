61a7a45b4142f8ee51ad0d610167511643b01eb4
Allan <allanjosueguzmanjarquin1@gmail.com>
2024-08-01 13:11:53 -0600

    Centinela completamente terminado

---
+++ centi.js
@@ -0,0 +1,14 @@
+function VerElNumero() {
+    const numero = parseInt(nume.value);
+
+//probe muchas combinaciones diferentes para usar el bucle de manera correcta, pero creo que igual no esta completamente bien.
+    while (numero) {
+        if (numero >= 40) {
+            window.alert("El numero ingresado es mayor a 40")
+            break;
+        } else {
+            window.alert("El numero ingresado es menor a 40")
+        }
+        break;
+    }
+}
